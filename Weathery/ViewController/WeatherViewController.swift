ad2f9fdd8d2f5af8aad75f0157d525da88238776
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-11-27 08:04:09 +0600

    UI components added.

--- Weathery/ViewController/WeatherViewController.swift
+++ Weathery/ViewController/WeatherViewController.swift
@@ -13,9 +13,19 @@ import UIKit
 class WeatherViewController: UIViewController {
     
     let backgroundView = UIImageView()
+    
+    let rootStackView = UIStackView()
+    
+    // Search
+    let searchStackView = UIStackView()
     let locationButton = UIButton()
     let searchButton = UIButton()
     let searchTextField = UITextField()
+    
+    // Weather
+    let conditionImageView = UIImageView()
+    let temperatureLabel = UILabel()
+    let cityLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +46,15 @@ extension WeatherViewController {
         backgroundView.image = UIImage(named: "background")
         backgroundView.contentMode = .scaleAspectFill
         
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.axis = .vertical
+        rootStackView.alignment = .trailing
+        rootStackView.spacing = 10
+        
+        // Search
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.spacing = 8
+        
         locationButton.translatesAutoresizingMaskIntoConstraints = false
         locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
         locationButton.tintColor = .label
@@ -50,14 +69,34 @@ extension WeatherViewController {
         searchTextField.textAlignment = .right
         searchTextField.borderStyle = .roundedRect
         searchTextField.backgroundColor = .systemFill
+        
+        // Weather
+        conditionImageView.translatesAutoresizingMaskIntoConstraints = false
+        conditionImageView.image = UIImage(systemName: "sun.max")
+        conditionImageView.tintColor = .label
+        
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.font = UIFont.systemFont(ofSize: 80)
+        temperatureLabel.text = "54.5"
+        
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        cityLabel.text = "Dhaka"
     }
     
     func layout() {
         
         view.addSubview(backgroundView)
-        view.addSubview(locationButton)
-        view.addSubview(searchButton)
-        view.addSubview(searchTextField)
+        view.addSubview(rootStackView)
+        
+        rootStackView.addArrangedSubview(searchStackView)
+        rootStackView.addArrangedSubview(conditionImageView)
+        rootStackView.addArrangedSubview(temperatureLabel)
+        rootStackView.addArrangedSubview(cityLabel)
+        
+        searchStackView.addArrangedSubview(locationButton)
+        searchStackView.addArrangedSubview(searchTextField)
+        searchStackView.addArrangedSubview(searchButton)
         
         NSLayoutConstraint.activate([
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -65,20 +104,31 @@ extension WeatherViewController {
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1), // multiplier 1 == 8 points
+            rootStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rootStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: rootStackView.trailingAnchor, multiplier: 1),
+            
+            searchStackView.widthAnchor.constraint(equalTo: rootStackView.widthAnchor),
+//            searchStackView.leadingAnchor.constraint(equalTo: rootStackView.leadingAnchor),
+//            searchStackView.trailingAnchor.constraint(equalTo: rootStackView.trailingAnchor),
+            
+//            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1), // multiplier 1 == 8 points
             locationButton.widthAnchor.constraint(equalToConstant: 40),
             locationButton.heightAnchor.constraint(equalToConstant: 40),
             
-            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
+//            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
 //            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8), // multiplier 1 == 8 points
             searchButton.widthAnchor.constraint(equalToConstant: 40),
             searchButton.heightAnchor.constraint(equalToConstant: 40),
             
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
-            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1)
+//            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
+//            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1),
+            
+            conditionImageView.heightAnchor.constraint(equalToConstant: 120),
+            conditionImageView.widthAnchor.constraint(equalToConstant: 120),
             
         ])
         
